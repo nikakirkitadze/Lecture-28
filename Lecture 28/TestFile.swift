88e2cf418e169187d2ed5121e64fe0b4924e442d
Nika Kirkitadze <nikakirkitadze@icloud.com>
2023-02-24 09:26:01 +0400

    Created new project, created new file, done small changes

---
+++ Lecture 28/TestFile.swift
@@ -0,0 +1,12 @@
+//
+//  TestFile.swift
+//  Lecture 28
+//
+//  Created by Nika Kirkitadze on 24.02.23.
+//
+
+import Foundation
+
+class TestClass {
+    
+}
---
+++ Lecture 28/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Lecture 28
+//
+//  Created by Nika Kirkitadze on 24.02.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    func testFunc() {
+        
+    }
+    
+    func newFunc() {
+        
+    }
+}
+
